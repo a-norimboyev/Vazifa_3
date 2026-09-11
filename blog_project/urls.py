a18ea2a998abf6806dcc15f2fd94b1e54527d0aa
Azizjon | Frontend Dev <norimboyevazizjon29@gmail.com>
2026-09-11 14:41:46 +0500

    chore: Clean media files, update gitignore and clean UI labels

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -31,6 +31,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+admin.site.site_header = "Blog Ma'muriyati (Admin)"
 admin.site.site_header = "Blog Ma'muriyati"
 admin.site.site_title = "Blog Admin Portali"
 admin.site.index_title = "Boshqaruv Paneli"
